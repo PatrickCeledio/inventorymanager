13ea83d1a31bcd1e594e51a9f81b911b6ddc62e7
Patrick Celedio <patrickceledio@gmail.com>
2021-01-12 20:10:14 -0800

    added basic index page

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const app = express();
+const expbs = require('express-handlebars');
+
+app.engine('handlebars', expbs());
+app.set('view engine', 'handlebars');
+
+// Routing
+app.get('', (req, res) => {
+    res.render('index');
+})
+
+app.listen(8080, () => {
+    console.log('SERVER IS STARTING AT PORT ', 8080);
+});
