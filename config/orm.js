263e5c322a42cba24054a13df91cb5301219c43b
Patrick Celedio <patrickceledio@gmail.com>
2021-01-13 02:07:56 -0800

    Resolved connection to MySQL

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,10 @@
+// All database behavior
 const connection = require('./connection')
 
 // In case there is an error, make the error the first print
 const orm = {
     selectAll: function(cb) {
-        connection.query("SELECT * FROM inventory_db", function (err, data) {
+        connection.query("SELECT * FROM all_inventory", function (err, data) {
             if (err) cb(err, null);
             cb(null, data);
         });
--- routes/handlers.js
+++ routes/handlers.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const orm = require("../config/orm")
 
 router.get("/", function(req, res) {
-    orm.selectAll(function (error, burgers) {
+    orm.selectAll(function (error, all_inventory) {
         if (error) {
             return res.status(501).json({
                 message: 'Unable to query the database'
